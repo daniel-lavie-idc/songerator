0f41e44c2290622bf4ac1796b14b392d6ed2e0ad
daniel-lavie <Daniel.Lavie@post.idc.ac.il>
2021-02-27 11:26:44 +0200

    Daniel morning session

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -1,4 +1,3 @@
-import { Scale } from "tone";
 import { generateNextNote, getLastScaleDegreePlayedByUser } from "./noteGenerator";
 
 
@@ -12,7 +11,7 @@ function hookStepSequencerUpdateCell() {
         const scaleDegree = stepSeq.rows - (row + 1); // We want to count the scale from bottom to top in the sequencer
         //we refer to the bottom row as the first row (instead of the top row)
         if (stepSeq._matrix[column][row] === false) {
-            console.log("User clicked note number: " + noteNumber + ", scale degree: " + scaleDegree); 
+            console.log("User clicked note number: " + noteNumber + ", scale degree: " + scaleDegree);
             window.NOTES_SELECTED_BY_USER.push(scaleDegree);
             if (noteNumber > window.MAX_NOTE_NUMBER_PLAYED) {
                 console.log(`New latest note number ${noteNumber} was detected, updating last (note) scale degree: ${scaleDegree}`);
@@ -20,6 +19,7 @@ function hookStepSequencerUpdateCell() {
                 window.LAST_SCALE_DEGREE_PLAYED_BY_USER = scaleDegree;
             }
         }
+        /* TODO: Decide if it's necessary
         else {
             //delete the note from the NOTES_SELECTED_BY_USER array
             const index = window.NOTES_SELECTED_BY_USER.indexOf(column);
@@ -27,71 +27,46 @@ function hookStepSequencerUpdateCell() {
                 window.NOTES_SELECTED_BY_USER.splice(index, 1);
             }
             //if we removed the far right note,update MAX_NOTE_NUMBER_PLAYED
-            if (column ===window.MAX_NOTE_NUMBER_PLAYED)
-            {
-                for ( let i in window.NOTES_SELECTED_BY_USER)
-                window.MAX_NOTE_NUMBER_PLAYED= Math.max(window.MAX_NOTE_NUMBER_PLAYED,i);
+            if (column === window.MAX_NOTE_NUMBER_PLAYED) {
+                for (let i in window.NOTES_SELECTED_BY_USER)
+                    window.MAX_NOTE_NUMBER_PLAYED = Math.max(window.MAX_NOTE_NUMBER_PLAYED, i);
             }
-        }
-        bindedOriginalUpdateCell(column,row);
+        }*/
+        bindedOriginalUpdateCell(column, row);
     };
 }
+
 // fills the rest of the sequencer
-function fillUp()
-{
+function fillUp() {
     console.log(`In fillUp function`);
     var stepSeq = document.querySelector("tone-step-sequencer");
     const scaleDegreeChosenByUser = getLastScaleDegreePlayedByUser();
     console.log(`fillUp::scaleDegreeChosenByUser: ${scaleDegreeChosenByUser}`);
-    let lastScaleDegree = generateNextNote(scaleDegreeChosenByUser);
+    let lastScaleDegree = scaleDegreeChosenByUser;
     let lastNoteNumber = window.MAX_NOTE_NUMBER_PLAYED + 1;
     do {
+        const nextNote = parseInt(generateNextNote(lastScaleDegree));
+        console.log(`fillUp:: next note = ${nextNote}`);
+        if (nextNote === -1) {
+            console.log("fillUp:: Got a silence note, continuing");
+            lastNoteNumber++;
+            continue;
+        }
+        lastScaleDegree = nextNote;
         console.log(`fillUp: Setting note number ${lastNoteNumber}, lastScaleDegree: ${lastScaleDegree}`);
         drawInSeq(lastNoteNumber, lastScaleDegree);
-        lastScaleDegree = generateNextNote(lastScaleDegree);
         lastNoteNumber++;
     } while (lastNoteNumber < stepSeq.columns)
-
-    /*
-    for (currentNoteNumber = lastNoteNumber; currentNoteNumber < 16; i++) {
-        drawInSeq(currentNoteNumber, newNote);
-        lastScaleDegree = generateNextNote(lastScaleDegree);
-    }
-
-
-
-    // Auto complete only fills up the right cells from where the user last played a note (stored in the variable `window.MAX_NOTE_NUMBER_PLAYED`)
-    for (let i=window.MAX_NOTE_NUMBER_PLAYED + 1; i < 16; i++)
-    {
-        let lastNote = getLastScaleDegreePlayedByUser();
-        let newNote = generateNextNote(lastNote);
-        //draw new note in the sequencer matrix
-        drawInSeq(i,newNote);
-    }
-    */
 }
 
 //TODO: Add sequencer as parameter in order to be able to use on all sequencers?
-function drawInSeq(column,row)
-{
+function drawInSeq(noteNumber, scaleDegree) {
     let stepSeq = document.querySelector("tone-step-sequencer");
-    stepSeq._matrix[column][row] = true;
+    console.log(`drawInSeq::scaleDegree = ${scaleDegree}`);
+    const rowInSequencer = stepSeq.rows - (parseInt(scaleDegree) + 1);
+    console.log(`drawInSeq::rowInSequencer = ${rowInSequencer}`);
+    stepSeq._matrix[noteNumber][rowInSequencer] = true;
     stepSeq.requestUpdate()
 }
 
-
-// function setNoteInSequencer() {
-//     var stepSeq = document.querySelector("tone-step-sequencer");
-//     if (MAX_NOTE_NUMBER_PLAYED === 15)
-//     return;
-//     let Note;
-//     //Fills the rest of the sequencer
-//     for (let i=MAX_NOTE_NUMBER_PLAYED; i<16; i ++)
-//     {
-//     note =generateNextNote();
-//     stepSeq._matrix[i][note]=true;
-//     stepSeq.render();
-//     }
-// }
-
-export {hookStepSequencerUpdateCell, fillUp};
+export { hookStepSequencerUpdateCell, fillUp };
--- src/noteGenerator.js
+++ src/noteGenerator.js
@@ -1,18 +1,18 @@
 const SILENCE_PROB = 0.2;
 const notesOccurrences = [
     // C    C#   D   D#   E    F    F#   G    G#   A   A#    B  
-    [1643, 77, 420, 70, 514, 116, 606, 232, 110, 554, 299, 649],
-    [86, 1750, 408, 156, 377, 152, 608, 157, 298, 277, 117, 700],
-    [589, 545, 2739, 26, 607, 156, 866, 581, 125, 425, 419, 665],
-    [80, 141, 21, 588, 293, 74, 263, 78, 127, 158, 58, 185],
-    [429, 372, 933, 308, 3203, 139, 1065, 741, 346, 591, 300, 850],
-    [120, 138, 137, 58, 207, 547, 263, 96, 113, 188, 88, 151],
-    [554, 386, 813, 340, 1275, 266, 4199, 931, 396, 1058, 823, 1474],
-    [304, 185, 374, 82, 703, 97, 1200, 2732, 130, 633, 406, 820],
-    [124, 394, 130, 94, 333, 153, 352, 131, 865, 316, 165, 345],
-    [603, 288, 563, 131, 449, 166, 976, 846, 386, 2954, 366, 1084],
-    [253, 101, 446, 45, 322, 96, 831, 394, 154, 398, 992, 544],
-    [504, 709, 759, 168, 994, 144, 1286, 748, 352, 1260, 543, 4083]
+    [1000, 77, 420, 70, 514, 116, 606, 232, 110, 554, 299, 649],        // C
+    [86, 1750, 408, 156, 377, 152, 608, 157, 298, 277, 117, 700],       // C#
+    [589, 545, 1200, 26, 607, 156, 866, 581, 125, 425, 419, 665],       // D
+    [80, 141, 21, 400, 293, 74, 263, 78, 127, 158, 58, 185],            // D#
+    [429, 372, 933, 308, 3203, 139, 1065, 741, 346, 591, 300, 850],     // E
+    [120, 138, 137, 58, 207, 400, 263, 96, 113, 188, 88, 151],          // F
+    [554, 386, 813, 340, 1275, 266, 4199, 931, 396, 1058, 823, 1474],   // F#
+    [304, 185, 374, 82, 703, 97, 1200, 1500, 130, 633, 406, 820],       // G
+    [124, 394, 130, 94, 333, 153, 352, 131, 600, 316, 165, 345],        // G#
+    [603, 288, 563, 131, 449, 166, 976, 846, 386, 2000, 366, 1084],     // A
+    [253, 101, 446, 45, 322, 96, 831, 394, 154, 398, 992, 544],         // A#
+    [504, 709, 759, 168, 994, 144, 1286, 748, 352, 1260, 543, 1700]     // B
 ];
 
 // C = 0, C# = 1, ..., B = 11
@@ -21,10 +21,11 @@ function generateNextNote(lastScaleDegreePlayed) {
     console.log("Current scale: " + currentScale);
 
     //scaleNotes gets the array of notes to choose from
-    const scaleNotes = getNotesInMajorScale(currentScale); // Array of notes
+    const scaleNotes = getNotesInMinorScale(currentScale); // Array of notes
     console.log("Scale notes: " + scaleNotes);
 
     // Assuming the user chose only a single note
+    lastScaleDegreePlayed = lastScaleDegreePlayed % (scaleNotes.length - 1); // If we got an octave or above, normalize the value
     console.log("Scale degree chosen by user: " + lastScaleDegreePlayed);
 
     // Indexing the note occurrences table according to the scale degree
@@ -34,10 +35,10 @@ function generateNextNote(lastScaleDegreePlayed) {
     let scaleDegreesToPosibillities = {};
     let currentScaleDegree = 0;
     for (let scaleNote of scaleNotes) {
-        scaleDegreesToPosibillities[currentScaleDegree] = currentNotePosibillities[scaleNote];
+        scaleDegreesToPosibillities[currentScaleDegree] = currentNotePosibillities[scaleNote % 12];
         currentScaleDegree++;
     }
-    
+
     console.log(`scaleDegreesToPosibillities: ${JSON.stringify(scaleDegreesToPosibillities)}`);
 
     var totalPossibilitySpace = 0;
@@ -46,19 +47,37 @@ function generateNextNote(lastScaleDegreePlayed) {
     }
     console.log(`totalPossibilitySpace: ${totalPossibilitySpace}`);
 
+    // Set silence probability, silence note will be represented by the value -1
+    scaleDegreesToPosibillities[-1] = calculateSilenceNumOfOccurences(totalPossibilitySpace);
+    console.log(`Silence number of occurences: ${scaleDegreesToPosibillities[-1]}`);
+    totalPossibilitySpace += scaleDegreesToPosibillities[-1];
+    console.log(`totalPossibilitySpace with silence: ${totalPossibilitySpace}`);
+
     const randomNumber = getRandomInt(totalPossibilitySpace);
     console.log(`Generated random number ${randomNumber}`);
     var sumOfOccurences = 0;
-    for (const scaleNote in scaleDegreesToPosibillities) {
+    for (let scaleNote in scaleDegreesToPosibillities) {
         const scaleNoteOccurences = scaleDegreesToPosibillities[scaleNote];
         console.log(`scaleNote ${scaleNote} occurences: ${scaleNoteOccurences}`);
         sumOfOccurences += scaleDegreesToPosibillities[scaleNote];
         if (randomNumber < sumOfOccurences) {
+            if (parseInt(scaleNote) === 0) {
+                // We got the root node, and it can be an octave above, so choosing randomaly between them
+                console.log("generateNextNote:: Returning a root node, so choosing randomly if it should be octave above");
+                if (Math.random() > 0.5) {
+                    console.log("generateNextNote:: returning the root octave higher");
+                    scaleNote = 7
+                }
+            }
             console.log(`Next note: ${scaleNote}`);
             return scaleNote;
         }
     };
     throw 'Could not generate next note!!';
+}
+
+function calculateSilenceNumOfOccurences(totalPossibilitySpace) {
+    return Math.floor((SILENCE_PROB / (1 - SILENCE_PROB)) * totalPossibilitySpace);
 }
 
 function getRandomInt(max) {
@@ -76,8 +95,7 @@ function getCurrentScale() {
 //     return window.NOTES_SELECTED_BY_USER[last];
 // }
 //returns the far right note that had been played 
-function getLastScaleDegreePlayedByUser()
-{
+function getLastScaleDegreePlayedByUser() {
     return window.LAST_SCALE_DEGREE_PLAYED_BY_USER;
 }
 
@@ -91,8 +109,18 @@ function getNotesInMajorScale(rootNote) {
         getNoteByInterval(rootNote, 11)];
 }
 
+function getNotesInMinorScale(rootNote) {
+    return [rootNote,
+        getNoteByInterval(rootNote, 2),
+        getNoteByInterval(rootNote, 3),
+        getNoteByInterval(rootNote, 5),
+        getNoteByInterval(rootNote, 7),
+        getNoteByInterval(rootNote, 8),
+        getNoteByInterval(rootNote, 10)];
+}
+
 function getNoteByInterval(originalNote, interval) {
-    return ((originalNote + interval) % 12);
+    return (originalNote + interval);
 }
 
 
